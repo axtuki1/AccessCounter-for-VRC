66a0bece96242488a09d0614fbbe1a27a8616fa2
axtuki1 <sakura.000.aki@gmail.com>
2022-11-01 19:24:32 +0900

    ゴミの掃除 #2

--- src/api/VRChatAccess.ts
+++ src/api/VRChatAccess.ts
@@ -4,8 +4,6 @@ const config = require('config');
 
 export class VRChatAccess extends API{
     
-    // https://script.google.com/macros/s/AKfycbx6eB3LO9YAPB2SKjJqyRHY1qOXMCt9LzkAKdMScFwz-j_kfgJP0zrYpyH0DcR5T0hHoQ/exec?mode=add&ipaddress=0.0.0.2
-
     public type: string = "get";
     public response = (req, res) => {
         const database_url = process.env.DATABASE_URL.replace("{IPADDR}", req.ip);
